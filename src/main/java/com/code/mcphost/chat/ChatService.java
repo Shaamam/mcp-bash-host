e52868f2c472e3d3cc0ff79a890664bc4f90d649
Shaamam <110484017+Shaamam@users.noreply.github.com>
2025-06-16 21:50:06 +0530

    Update application properties and ChatService to configure MCP client connections

--- src/main/java/com/code/mcphost/chat/ChatService.java
+++ src/main/java/com/code/mcphost/chat/ChatService.java
@@ -3,15 +3,10 @@ package com.code.mcphost.chat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,9 +22,13 @@ public class ChatService {
 
         ToolCallback[] toolCallbacks = toolCallbackProvider.getToolCallbacks();
 
-        return new ChatBotResponse(question, ChatClient.create(openAiChatModel)
+        ChatClient chatClient = ChatClient.builder(openAiChatModel)
+                .defaultToolCallbacks(toolCallbacks)
+                .build();
+
+
+        return new ChatBotResponse(question, chatClient
                 .prompt(question)
-                .tools(toolCallbacks)
                 .call()
                 .content());
     }
